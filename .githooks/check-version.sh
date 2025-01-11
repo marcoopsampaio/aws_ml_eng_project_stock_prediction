b269cb50fd36438b5d6414ac3b594de088db9260
Marco Sampaio <marcoopsampaio@gmail.com>
2025-01-11 15:23:07 +0000

    Adds project proposal file to the docs

--- .githooks/check-version.sh
+++ .githooks/check-version.sh
@@ -4,21 +4,13 @@
 git fetch --tags -q
 git fetch origin main -q
 
-MAIN_PACKAGE_VERSION=$(git ls-remote --tags origin | \
-                      awk '{print $2}' | \
-                      grep 'refs/tags' | \
-                      sed 's/refs\/tags\/v//' | \
-                      grep -v '{}' | \
-                      sort -V | \
-                      tail -n 1)
-
 CURRENT_PACKAGE_VERSION=$(poetry version | awk '{print $2}')
 
-if [[ "$CURRENT_PACKAGE_VERSION" > "$MAIN_PACKAGE_VERSION" ]]
+if git tag -l | grep -q "v$CURRENT_PACKAGE_VERSION"
 then
-  echo "Version good to go"
-  exit 0
-else
   echo "Please bump version prior to commit"
   exit 1
+else
+  echo "Version good to go"
+  exit 0
 fi
